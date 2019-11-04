ea7b925e720cee1aadd9dcba4cea22c808c89fed
emmaj <emmaj@siminn.is>
2019-11-04 23:12:00 +0000

    búin að breyta öllu

---
+++ src/main/java/project/controller/HelloController.java
@@ -0,0 +1,16 @@
+package project.controller;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+
+        return "Index";
+    }
+    
+}
---
+++ src/webapp/WEB-INF/jsp/Index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html lang="en">
+    <head>
+        <title>Memory Game</title>
+    </head>
+    <body>
+    <h1> Memory Game Website</h1>
+    <p>Þetta mun vera beinagrindin</p>
+    </body>
+</html>
+
